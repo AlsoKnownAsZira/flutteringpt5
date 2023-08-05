cb5ce9b6d30d75ae53025bed75b946f200079cd3
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-08-05 23:26:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutteringpt5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutteringpt5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ziraApp());
+}
+
+class ziraApp extends StatelessWidget {
+  const ziraApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
